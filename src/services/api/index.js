75d0478a402da2a00cfce2399449d0f2278e1dbf
Juan Diego Rojas Aguilar <yo217@hotmail.es>
2022-12-21 12:12:48 -0500

    [FIX] api/index.js commit error

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -8,11 +8,7 @@ const endPoints = {
 		profile: `${API}/${VERSION}/auth/profile`,
 	},
 	products: {
-<<<<<<< HEAD
-		allproducts: `${API}/${VERSION}/products/`,
-=======
 		allProducts: `${API}/${VERSION}/products`,
->>>>>>> 733350906c88aea82acda082481e04f78e51c4ad
 		getProduct: (id) => `${API}/${VERSION}/products/${id}`,
 		getProducts: (limit, offset) => `${API}/${VERSION}/products?limit=${limit}&offset=${offset}`,
 		updateProducts: (id) => `${API}/${VERSION}/products/${id}/`,
